deb3cc50907ea726cf2aeffb5fb69a6437d82b44
YdrMaster <ydrml@hotmail.com>
2025-03-28 18:14:30 +0800

    feat: 格式化打印移动到 array layout
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/fmt.rs
+++ src/fmt.rs
@@ -2,124 +2,60 @@
 use digit_layout::types;
 use std::fmt;
 
-pub trait DataFmt {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result;
-}
-
-impl DataFmt for f32 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if *self == 0. {
-            write!(f, " ________")
-        } else {
-            write!(f, "{self:>9.3e}")
-        }
-    }
-}
-
-impl DataFmt for f64 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if *self == 0. {
-            write!(f, " ________")
-        } else {
-            write!(f, "{self:>9.3e}")
-        }
-    }
-}
-
-impl DataFmt for u32 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if *self == 0 {
-            write!(f, " ________")
-        } else {
-            write!(f, "{self:>6}")
-        }
-    }
-}
-
-impl DataFmt for u64 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if *self == 0 {
-            write!(f, " ________")
-        } else {
-            write!(f, "{self:>6}")
-        }
-    }
-}
-
 impl fmt::Display for Tensor<&[u8]> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let layout = self.layout();
+        let ptr = self.get().as_ptr();
         match self.dt() {
-            types::F32 => self.write_tensor::<f32>(&mut vec![], f),
-            types::F64 => self.write_tensor::<f64>(&mut vec![], f),
-            types::U32 => self.write_tensor::<u32>(&mut vec![], f),
-            types::U64 => self.write_tensor::<u64>(&mut vec![], f),
+            types::F32 => unsafe { layout.write_array(f, ptr.cast::<DataFmt<f32>>()) },
+            types::F64 => unsafe { layout.write_array(f, ptr.cast::<DataFmt<f64>>()) },
+            types::U32 => unsafe { layout.write_array(f, ptr.cast::<DataFmt<u32>>()) },
+            types::U64 => unsafe { layout.write_array(f, ptr.cast::<DataFmt<u64>>()) },
             _ => todo!(),
         }
     }
 }
 
-impl Tensor<&[u8]> {
-    fn write_tensor<T: DataFmt>(
-        &self,
-        indices: &mut Vec<[usize; 2]>,
-        f: &mut fmt::Formatter,
-    ) -> fmt::Result {
-        let ptr = self.ptr::<T>();
-        match *self.shape() {
-            [] => {
-                write!(f, "<>")?;
-                write_indices(f, indices)?;
-                write_matrix(f, ptr, [1, 1], [1, 1])
-            }
-            [len] => {
-                let &[stride] = self.layout().strides() else {
-                    unreachable!()
-                };
-                write!(f, "<{len}>")?;
-                write_indices(f, indices)?;
-                write_matrix(f, ptr, [len, 1], [stride, 1])
-            }
-            [rows, cols] => {
-                let &[rs, cs] = self.layout().strides() else {
-                    unreachable!()
-                };
-                write!(f, "<{rows}x{cols}>")?;
-                write_indices(f, indices)?;
-                write_matrix(f, ptr, [rows, cols], [rs, cs])
-            }
-            [batch, ..] => {
-                for i in 0..batch {
-                    indices.push([i, batch]);
-                    self.as_slice().index(&[i]).write_tensor::<T>(indices, f)?;
-                    indices.pop();
-                }
-                Ok(())
-            }
+#[derive(Clone, Copy)]
+#[repr(transparent)]
+pub struct DataFmt<T: Copy>(T);
+
+impl fmt::Display for DataFmt<f32> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.0 == 0. {
+            write!(f, " ________")
+        } else {
+            write!(f, "{:>9.3e}", self.0)
         }
     }
 }
 
-fn write_matrix<T: DataFmt>(
-    f: &mut fmt::Formatter,
-    ptr: *const T,
-    shape: [usize; 2],
-    strides: [isize; 2],
-) -> fmt::Result {
-    let [rows, cols] = shape;
-    let [rs, cs] = strides;
-    for r in 0..rows as isize {
-        for c in 0..cols as isize {
-            unsafe { &*ptr.byte_offset(r * rs + c * cs) }.fmt(f)?;
-            write!(f, " ")?;
+impl fmt::Display for DataFmt<f64> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.0 == 0. {
+            write!(f, " ________")
+        } else {
+            write!(f, "{:>9.3e}", self.0)
         }
-        writeln!(f)?;
     }
-    Ok(())
 }
 
-fn write_indices(f: &mut fmt::Formatter, indices: &[[usize; 2]]) -> fmt::Result {
-    for &[i, b] in indices {
-        write!(f, ", {i}/{b}")?;
+impl fmt::Display for DataFmt<u32> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.0 == 0 {
+            write!(f, " ________")
+        } else {
+            write!(f, "{:>6}", self.0)
+        }
     }
-    writeln!(f)
+}
+
+impl fmt::Display for DataFmt<u64> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.0 == 0 {
+            write!(f, " ________")
+        } else {
+            write!(f, "{:>6}", self.0)
+        }
+    }
 }
