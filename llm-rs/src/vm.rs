3d7fb6e7c8599d6137ffd2a1ed467a137684a1a8
pwhMass <a203865235@gmail.com>
2025-04-10 05:27:59 +0800

    feat: 为tensor增加tile等操作（从infiniNN移植）

--- llm-rs/src/vm.rs
+++ llm-rs/src/vm.rs
@@ -2,6 +2,11 @@
 use digit_layout::DigitLayout;
 use rw_rc::RwRc;
 
+// 这个模块定义了虚拟机和张量的trait以及相关实现
+// VirtualMachine trait定义了创建张量的方法
+// Tensor trait定义了对张量进行操作的方法
+// 实现基于transform.rs中的方法，但做了适当调整以匹配trait的接口要求
+
 pub trait VirtualMachine {
     type Tensor: Tensor;
 
@@ -13,6 +18,11 @@ pub trait Tensor {
     fn dt(&self) -> DigitLayout;
     fn shape(&self) -> &[usize];
     fn merge(&self, axis: usize, len: usize) -> Self;
+    fn tile(&self, axis: usize, tiles: &[usize]) -> Self;
+    fn broadcast(&self, axis: usize, times: usize) -> Self;
+    fn transpose(&self, perm: &[usize]) -> Self;
+    fn slice(&self, axis: usize, start: usize, len: usize) -> Self;
+    fn split<'a>(&'a self, axis: usize, parts: &'a [usize]) -> impl Iterator<Item = Self> + 'a;
 }
 
 pub struct TestVM;
@@ -40,6 +50,26 @@ impl Tensor for crate::nn::Tensor_ {
 
     fn merge(&self, axis: usize, len: usize) -> Self {
         self.clone().merge(axis, len)
+    }
+
+    fn tile(&self, axis: usize, tiles: &[usize]) -> Self {
+        self.clone().tile(axis, tiles)
+    }
+
+    fn broadcast(&self, axis: usize, times: usize) -> Self {
+        self.clone().broadcast(axis, times)
+    }
+
+    fn transpose(&self, perm: &[usize]) -> Self {
+        self.clone().transpose(perm)
+    }
+
+    fn slice(&self, axis: usize, start: usize, len: usize) -> Self {
+        self.clone().slice(axis, start, len)
+    }
+
+    fn split<'a>(&'a self, axis: usize, parts: &'a [usize]) -> impl Iterator<Item = Self> + 'a {
+        self.split(axis, parts)
     }
 }
 
